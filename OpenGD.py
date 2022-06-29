8e5e035d4431a22f3303d13755985f71c4bd85c6
SHI3DO <61821471+SHI3DO@users.noreply.github.com>
2022-06-29 11:17:06 +0900

    a

--- OpenGD.py
+++ OpenGD.py
@@ -1,10 +1,12 @@
 import os
+import random
 from array import array
 
 import glfw
 import numpy
 
 from OpenGL.GL import *
+from OpenGL.GLU import *
 from Utils import jsonreader
 from PIL import Image
 
@@ -16,7 +18,7 @@ def load_file(filename):
 
 # //////////////////////////////////////////////////////////////////////////////
 config = jsonreader.get(load_file("Config/config.json"))
-fps = 240
+fps = int(config.fps)
 # //////////////////////////////////////////////////////////////////////////////
 ICON_image = Image.open(load_file("Icon/OPENGD.ico"))
 
@@ -52,12 +54,13 @@ def main(window_width, window_height, dev=True):
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0)
+        print(texx.size[0], texx.size[1])
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texx.size[0], texx.size[1], 0, GL_RGBA, GL_UNSIGNED_BYTE, texxData)
         texx.close()
         return texxID
 
-    def drawQuad(centerX, centerY, textureID):
-        verts = ((1, 1), (1, -1), (-1, -1), (-1, 1))
+    def drawQuad(centerX, centerY, textureID, ratio):
+        verts = ((ratio, ratio), (ratio, -ratio), (-ratio, -ratio), (-ratio, ratio))
         texts = ((1, 0), (1, 1), (0, 1), (0, 0))
         surf = (0, 1, 2, 3)
 
@@ -72,21 +75,25 @@ def main(window_width, window_height, dev=True):
         glDisable(GL_TEXTURE_2D)
 
     # //////////////////////////////////////////////////////////////////////////////
-    bg_0_0_0_0_0_0_textureID = loadTexture(load_file("Resources/bg_0_0_0_0_0_0.png"))
-    # //////////////////////////////////////////////////////////////////////////////
-    glViewport(0, 0, window_width, window_height)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
+    sample_256_textureID = loadTexture(load_file("Resources/sample_256.png"))
+    bg_textureID = loadTexture(load_file("Resources/bg_0_0_0_0_0_0.png"))
+
     # //////////////////////////////////////////////////////////////////////////////
     last_time = glfw.get_time()
     fps_previoustime = glfw.get_time()
     fps_framecount = 0
     # //////////////////////////////////////////////////////////////////////////////
+    gluPerspective(45, (16 / 9), 0.1, 50.0)
+    glTranslatef(0, 0, -5)
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    # //////////////////////////////////////////////////////////////////////////////
     while not glfw.window_should_close(window):
         glfw.poll_events()
 
-        glClear(GL_COLOR_BUFFER_BIT)
-        drawQuad(0, 0, bg_0_0_0_0_0_0_textureID)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        drawQuad(0, -1.4, bg_textureID, 6)
+        drawQuad(glfw.get_time() - 12, -1.4, sample_256_textureID, 1)
 
         glfw.swap_buffers(window)
         # //////////////////////////////////////////////////////////////////////////////
@@ -106,6 +113,7 @@ def main(window_width, window_height, dev=True):
         # //////////////////////////////////////////////////////////////////////////////
         v_width, v_height = glfw.get_window_size(window)
         glViewport(0, 0, v_width, v_height)
+
     glfw.destroy_window(window)
     glfw.terminate()
 
